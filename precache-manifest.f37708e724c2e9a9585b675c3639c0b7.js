876ac2081c86e788049500a33ec9169962951047
Mohak Chaudhari <mohak.672@gmail.com>
2020-04-21 19:18:30 +0530

    Updates

---
+++ precache-manifest.f37708e724c2e9a9585b675c3639c0b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e157d6d145c5024935aab838e7d1553",
+    "url": "/robo-friends-udemy-react-app/index.html"
+  },
+  {
+    "revision": "dc8318b9de318c267c9c",
+    "url": "/robo-friends-udemy-react-app/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "cfe9d5d5f82811dc289e",
+    "url": "/robo-friends-udemy-react-app/static/css/main.1f25c0fe.chunk.css"
+  },
+  {
+    "revision": "dc8318b9de318c267c9c",
+    "url": "/robo-friends-udemy-react-app/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends-udemy-react-app/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe9d5d5f82811dc289e",
+    "url": "/robo-friends-udemy-react-app/static/js/main.e1c43e97.chunk.js"
+  },
+  {
+    "revision": "4daaed2e9284df5bcf44",
+    "url": "/robo-friends-udemy-react-app/static/js/runtime-main.c32865df.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends-udemy-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
